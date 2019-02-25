06dfd5b8fa70bc17a9cfcbf3b1f5966a17798cd6
gnehcgnaw <949174604@qq.com>
2019-02-25 14:26:39 +0800

    初始化项目，添加.gitihnore文件

---
+++ src/main/java/red/reksai/nettysamples/NettySamplesApplication.java
@@ -0,0 +1,13 @@
+package red.reksai.nettysamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettySamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettySamplesApplication.class, args);
+    }
+
+}
